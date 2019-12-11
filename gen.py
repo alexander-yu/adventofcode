58d21f548163b37e5da867b02a1bd29fb14918d9
Alex Yu <yu.alex96@gmail.com>
2019-12-11 17:27:42 -0500

    code gen script for generating problem templates

---
+++ gen.py
@@ -0,0 +1,49 @@
+import datetime
+import os
+
+import click
+
+TEMPLATE = """
+import click
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def part_1():
+    pass
+
+
+@cli.command()
+def part_2():
+    pass
+
+
+if __name__ == '__main__':
+    cli()
+"""
+
+
+@click.command()
+@click.argument('problem', nargs=1)
+@click.option('-y', '--year', type=int)
+def cli(problem, year):
+    year = year if year else datetime.datetime.now().year
+
+    directory = os.path.join(os.getcwd(), f'problems_{year}')
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    path = os.path.join(directory, f'{problem}.py')
+    if os.path.exists(path):
+        raise Exception(f'File {path} already exists')
+
+    with open(path, 'w') as f:
+        f.write(TEMPLATE)
+
+
+if __name__ == '__main__':
+    cli()  # pylint: disable=no-value-for-parameter
