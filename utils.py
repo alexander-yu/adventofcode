12a0a0206dfc1d8871f72054d22253feaf7c774a
Alex Yu <yu.alex96@gmail.com>
2019-12-11 14:44:37 -0500

    Fix utils

--- utils.py
+++ utils.py
@@ -8,11 +8,12 @@ def _split_line(line, delimiter):
 
 
 def get_input(test=False, delimiter=',', cast=int):
-    problem_number = pathlib.Path(sys.argv[0]).stem
+    path = pathlib.Path(sys.argv[0])
+    problem_number = path.stem
     test_prefix = '_test' if test else ''
     input_file_name = f'{problem_number}{test_prefix}.txt'
 
-    with open(os.path.join(os.getcwd(), 'inputs', input_file_name), 'r') as f:
+    with open(os.path.join(path.parent, 'inputs', input_file_name), 'r') as f:
         lines = f.read().strip().split('\n')
 
     return [
