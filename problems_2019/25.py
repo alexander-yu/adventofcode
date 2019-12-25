73e474cb011b0b7a7c4d19d333bb3d25dfee0c5b
Alex Yu <yu.alex96@gmail.com>
2019-12-25 00:34:19 -0500

    Add actual answer

--- problems_2019/25.py
+++ problems_2019/25.py
@@ -5,9 +5,11 @@ import utils
 from problems_2019 import intcode
 
 
-def run():
+def run(commands=None):
     memory = utils.get_input(__file__)[0]
-    program = intcode.Program(memory, output_mode=intcode.OutputMode.BUFFER)
+    initial_inputs = intcode.commands_to_input(commands or [])
+    program = intcode.Program(memory, initial_inputs=initial_inputs, output_mode=intcode.OutputMode.BUFFER)
+
     while True:
         _, return_signal = program.run()
         for output in program.yield_outputs():
@@ -17,6 +19,7 @@ def run():
                 print(output)
 
         if return_signal == intcode.ReturnSignal.AWAITING_INPUT:
+            # Run in interactive mode if more commands needed
             program.add_inputs(*intcode.commands_to_input([input()]))
         elif return_signal == intcode.ReturnSignal.RETURN_AND_HALT:
             return
@@ -31,7 +34,32 @@ def cli():
 
 @cli.command()
 def part_1():
-    run()
+    commands = [
+        'south',
+        'take food ration',
+        'west',
+        'north',
+        'north',
+        'east',
+        'take astrolabe',
+        'west',
+        'south',
+        'south',
+        'east',
+        'north',
+        'east',
+        'south',
+        'take weather machine',
+        'west',
+        'take ornament',
+        'east',
+        'north',
+        'east',
+        'east',
+        'east',
+        'south',
+    ]
+    run(commands=commands)
 
 
 if __name__ == '__main__':
