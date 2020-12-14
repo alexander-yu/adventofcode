0cdf64b1d0ed707537e8b13e2c6d0de079f3babc
Alex Yu <yu.alex96@gmail.com>
2020-12-14 01:17:20 -0500

    [2020.14] Solution

---
+++ problems_2020/14.py
@@ -0,0 +1,110 @@
+import itertools
+import re
+
+import click
+
+import utils
+
+
+WRITE_REGEX = r'mem\[(?P<address>\d+)\]'
+
+
+class Mask:
+    def __init__(self, bits, version):
+        self.bits = dict(enumerate(reversed(bits)))
+        self.version_handlers = {
+            1: self._apply_v1,
+            2: self._apply_v2,
+        }
+        if version not in self.version_handlers:
+            raise ValueError(
+                f'Expected version to be one of {[self.version_handlers.keys()]}, got: {version}'
+            )
+        self.version_handler = self.version_handlers[version]
+
+    @staticmethod
+    def _set_bit(value, index, bit):
+        if bit:
+            return value | (1 << index)
+        return value & ~(1 << index)
+
+    def _apply_v1(self, memory, address, value):
+        for i, bit in self.bits.items():
+            if bit != 'X':
+                value = self._set_bit(value, i, int(bit))
+            else:
+                continue
+
+        memory[address] = value
+
+    def _apply_v2(self, memory, address, value):
+        floating_bits = []
+
+        for i, bit in self.bits.items():
+            if bit == '1':
+                address = self._set_bit(address, i, 1)
+            elif bit == '0':
+                continue
+            else:
+                floating_bits.append(i)
+
+        if floating_bits:
+            floating_addresses = self._resolve_floating_bits(address, floating_bits)
+            for floating_address in floating_addresses:
+                memory[floating_address] = value
+        else:
+            memory[address] = value
+
+    def _resolve_floating_bits(self, floating_address, floating_bits):
+        addresses = []
+        bit_combinations = itertools.product([0, 1], repeat=len(floating_bits))
+
+        for bits in bit_combinations:
+            address = floating_address
+
+            for i, bit in zip(floating_bits, bits):
+                address = self._set_bit(address, i, bit)
+
+            addresses.append(address)
+
+        return addresses
+
+    def apply(self, memory, address, value):
+        return self.version_handler(memory, address, value)
+
+
+def run_program(version):
+    instructions = utils.get_input(__file__, delimiter=' = ', cast=str)
+    memory = {}
+    mask = None
+
+    for instruction, value in instructions:
+        match = re.fullmatch(WRITE_REGEX, instruction)
+        if match:
+            address = int(match.group('address'))
+            mask.apply(memory, address, int(value))
+        else:
+            mask = Mask(list(value), version)
+
+    return memory
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def part_1():
+    memory = run_program(1)
+    print(sum(memory.values()))
+
+
+@cli.command()
+def part_2():
+    memory = run_program(2)
+    print(sum(memory.values()))
+
+
+if __name__ == '__main__':
+    cli()
