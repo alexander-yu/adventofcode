8de43f4a34476fa4554533c05032d072001a950a
Alex Yu <yu.alex96@gmail.com>
2020-12-15 00:19:40 -0500

    [2020.15] Solution

---
+++ problems_2020/15.py
@@ -0,0 +1,45 @@
+import collections
+
+import click
+
+import utils
+
+
+def play(last_round):
+    numbers = utils.get_input(__file__)[0]
+
+    occurrences = collections.defaultdict(list, {number: [i] for i, number in enumerate(numbers)})
+    last_number = numbers[-1]
+
+    i = len(numbers)
+
+    while i < last_round:
+        if len(occurrences[last_number]) == 1:
+            last_number = 0
+        else:
+            last_two_occurrences = occurrences[last_number][-2:]
+            last_number = last_two_occurrences[1] - last_two_occurrences[0]
+
+        occurrences[last_number].append(i)
+        i += 1
+
+    print(last_number)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def part_1():
+    play(2020)
+
+
+@cli.command()
+def part_2():
+    play(30000000)
+
+
+if __name__ == '__main__':
+    cli()
