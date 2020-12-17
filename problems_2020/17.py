376f16321edfb4722dde342fbeae3ae046caf269
Alex Yu <yu.alex96@gmail.com>
2020-12-17 00:35:56 -0500

    [2020.17] Solution

---
+++ problems_2020/17.py
@@ -0,0 +1,87 @@
+import itertools
+
+import click
+
+import utils
+
+
+def get_neighbors(point):
+    dims = len(point)
+    zero = tuple(0 for _ in range(dims))
+
+    for vector in itertools.product([-1, 0, 1], repeat=dims):
+        if vector != zero:
+            yield utils.add_vector(point, vector)
+
+
+class PocketDimension:
+    def __init__(self, active_points):
+        self.active_points = set(active_points)
+
+    def _get_potential_changes(self):
+        potential_changes = set()
+
+        for point in self.active_points:
+            potential_changes.add(point)
+
+            for neighbor in get_neighbors(point):
+                potential_changes.add(neighbor)
+
+        return potential_changes
+
+    def _run_cycle(self):
+        new_active_points = set()
+
+        for point in self._get_potential_changes():
+            active_neighbors = len([
+                neighbor
+                for neighbor in get_neighbors(point)
+                if neighbor in self.active_points
+            ])
+
+            if point in self.active_points and active_neighbors in [2, 3]:
+                new_active_points.add(point)
+            elif point not in self.active_points and active_neighbors == 3:
+                new_active_points.add(point)
+
+        self.active_points = new_active_points
+
+    def boot_up(self):
+        for _ in range(6):
+            self._run_cycle()
+
+
+def get_pocket_dimension(dims):
+    points = utils.get_input(__file__, delimiter='', cast=str)
+    active_points = set()
+
+    for i, row in enumerate(points):
+        for j, status in enumerate(row):
+            if status == '#':
+                point = (i, j) + tuple(0 for _ in range(dims - 2))
+                active_points.add(point)
+
+    return PocketDimension(active_points)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def part_1():
+    pocket_dimension = get_pocket_dimension(3)
+    pocket_dimension.boot_up()
+    print(len(pocket_dimension.active_points))
+
+
+@cli.command()
+def part_2():
+    pocket_dimension = get_pocket_dimension(4)
+    pocket_dimension.boot_up()
+    print(len(pocket_dimension.active_points))
+
+
+if __name__ == '__main__':
+    cli()
