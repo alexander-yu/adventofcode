230b7195bcab0de317499dc40e1b3ffd012f0efc
Alex Yu <yu.alex96@gmail.com>
2020-12-25 00:18:15 -0500

    [2020.25] Solution

---
+++ problems_2020/25.py
@@ -0,0 +1,35 @@
+import click
+
+import utils
+
+
+def get_loop(subject, key, mod):
+    power = 1
+
+    for i in range(mod):
+        power *= subject
+        power %= mod
+        if power == key:
+            return i + 1
+
+    raise ValueError(
+        f'Could not find loop size for subject {subject}, key {key}, and mod {mod}'
+    )
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def part_1():
+    card_key, door_key = utils.get_input(__file__, delimiter=None)
+    subject, mod = 7, 20201227
+    card_loop = get_loop(subject, card_key, mod)
+    door_loop = get_loop(subject, door_key, mod)
+    print(pow(subject, card_loop * door_loop, mod=mod))
+
+
+if __name__ == '__main__':
+    cli()
