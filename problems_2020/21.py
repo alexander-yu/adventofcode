012e24ede8dc17f10a4e7a95d7fb282d698983a2
Alex Yu <yu.alex96@gmail.com>
2021-11-27 01:15:34 -0500

    Add z3 attempt for 2020.21.2

--- problems_2020/21.py
+++ problems_2020/21.py
@@ -5,20 +5,21 @@ import typing
 from dataclasses import dataclass
 
 import click
+import z3
 
 from boltons import iterutils
 
 import utils
 
 
-@dataclass
+@dataclass(frozen=True)
 class Food:
-    ingredients: typing.Set[str]
-    allergens: typing.Set[str]
+    ingredients: typing.FrozenSet[str]
+    allergens: typing.FrozenSet[str]
 
     @staticmethod
     def parse(ingredients_str, allergens_str):
-        return Food(set(ingredients_str.split(' ')), set(allergens_str.split(', ')))
+        return Food(frozenset(ingredients_str.split(' ')), frozenset(allergens_str.split(', ')))
 
 
 def get_foods():
@@ -105,6 +106,61 @@ def part_2():
 
     allergens = sorted(allergen_map.keys())
     print(','.join(allergen_map[allergen] for allergen in allergens))
+
+
+@cli.command()
+def part_2_z3():
+    foods = get_foods()
+    hypoallergenic = get_hypoallergenic_ingredients(foods)
+
+    allergens = set()
+    ingredients = set()
+    foods_by_allergen = collections.defaultdict(set)
+    foods_by_ingredient = collections.defaultdict(set)
+
+    for food in foods:
+        for allergen in food.allergens:
+            foods_by_allergen[allergen].add(food)
+            allergens.add(allergen)
+
+        for ingredient in food.ingredients:
+            foods_by_ingredient[ingredient].add(food)
+            ingredients.add(ingredient)
+
+    allergens = list(allergens)
+    ingredients = list(ingredients - hypoallergenic)
+
+    # List of variables representing possible assignment of ingredient to allergen
+    assignments = z3.IntVector('assignment', len(ingredients))
+    solver = z3.Solver()
+
+    for assignment in assignments:
+        solver.add(0 <= assignment)
+        solver.add(assignment < len(allergens))
+
+    solver.add(z3.Distinct(assignments))
+
+    for i, allergen in enumerate(allergens):
+        candidates = []
+
+        for j, ingredient in enumerate(ingredients):
+            # If set of foods that we know contain allergen_i is a subset of foods containing ingredient_j,
+            # then ingredient_j = allergen_i is a possible assignment
+            if foods_by_allergen[allergen] <= foods_by_ingredient[ingredient]:
+                candidates.append(assignments[j] == i)
+
+        solver.add(z3.Or(candidates))
+
+    assert solver.check() == z3.sat
+
+    model = solver.model()
+    matches = []
+
+    for i, assignment in enumerate(assignments):
+        assignment = model.evaluate(assignment).as_long()
+        matches.append((allergens[assignment], ingredients[i]))
+
+    print(','.join(ingredient for _, ingredient in sorted(matches)))
 
 
 if __name__ == '__main__':
