dd46b04ca36d7fedf42d6651a8a1c5456ecd8a19
Alex Yu <yu.alex96@gmail.com>
2021-12-01 00:06:56 -0500

    [2021] Day 1

---
+++ problems_2021/1.py
@@ -0,0 +1,38 @@
+import click
+
+import utils
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@utils.part(__name__, 1)
+def part_1():
+    depths = utils.get_input(__file__, delimiter=None)
+
+    increases = sum(
+        depths[i] - depths[i - 1] > 0
+        for i in range(1, len(depths))
+    )
+
+    print(increases)
+
+
+@cli.command()
+@utils.part(__name__, 2)
+def part_2():
+    depths = utils.get_input(__file__, delimiter=None)
+
+    increases = sum(
+        depths[i] - depths[i - 3] > 0
+        for i in range(3, len(depths))
+    )
+
+    print(increases)
+
+
+if __name__ == '__main__':
+    cli()
