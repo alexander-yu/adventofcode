84c6a728e8ed9fb4974dd4e6c357dcd67d06d716
Alex Yu <yu.alex96@gmail.com>
2021-12-19 23:37:03 -0700

    [2021.20] Generalized void logic

--- problems_2021/20.py
+++ problems_2021/20.py
@@ -9,13 +9,21 @@ import utils
 KERNEL = np.reshape(2 ** np.arange(9), (3, 3))
 
 
-def enhance_image(iters):
+def get_void(enhancement, iteration):
+    if iteration == 0 or enhancement[0] == '.':
+        return 0
+    if enhancement[-1] == '#':
+        return 1
+    return iteration % 2
+
+
+def enhance_image(iterations):
     enhancement, image = utils.get_input(__file__, delimiter=None, line_delimiter='\n\n', cast=str)
     image = utils.parse(image, delimiter='', cast=lambda pixel: int(pixel == '#'))
     light = [i for i, ch in enumerate(enhancement) if ch == '#']
 
-    for i in range(iters):
-        image = signal.convolve2d(image, KERNEL, fillvalue=i % 2)
+    for i in range(iterations):
+        image = signal.convolve2d(image, KERNEL, fillvalue=get_void(enhancement, i))
         image = np.isin(image, light).astype(int)
 
     return image
