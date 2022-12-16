f1d224342e561b3c8133fdb532cb6942a541a339
Alex Yu <yu.alex96@gmail.com>
2022-12-16 03:46:04 -0500

    [2022.16] Dirty DFS solution

---
+++ problems_2022/16.py
@@ -0,0 +1,142 @@
+import re
+
+import utils
+
+
+def get_data():
+    matches = utils.get_input(
+        cast=lambda line: re.match(
+            r'Valve (?P<valve>.+) has flow rate=(?P<rate>\d+); tunnels? leads? to valves? (?P<next_valves>.+)',
+            line,
+        ),
+        delimiter=None,
+        line_delimiter='\n',
+    )
+    data = []
+
+    for match in matches:
+        data.append((
+            match.group('valve'),
+            int(match.group('rate')),
+            match.group('next_valves').split(', '),
+        ))
+
+    neighbors = {}
+    rates = {}
+    valves_to_open = set()
+
+    for valve, rate, valve_neighbors in data:
+        neighbors[valve] = valve_neighbors
+        rates[valve] = rate
+
+        if rate:
+            valves_to_open.add(valve)
+
+    return neighbors, rates, frozenset(valves_to_open)
+
+
+@utils.part
+def part_1():
+    neighbors, rates, _ = get_data()
+
+    open_valves = set()
+    seen = {}
+    max_so_far = 0
+
+    def search(time, me, pressure):
+        nonlocal max_so_far
+
+        if seen.get((time, me), -1) >= pressure:
+            return
+
+        seen[time, me] = pressure
+
+        if time == 30:
+            max_so_far = max(max_so_far, pressure)
+            return
+
+        net_pressure = sum(rates[valve] for valve in open_valves)
+
+        if me not in open_valves and rates[me]:
+            open_valves.add(me)
+            search(time + 1, me, pressure + net_pressure + rates[me])
+            open_valves.remove(me)
+
+        for neighbor in neighbors[me]:
+            search(time + 1, neighbor, pressure + net_pressure)
+
+    search(1, 'AA', 0)
+    print(max_so_far)
+
+
+@utils.part
+def part_2():
+    neighbors, rates, valves_to_open = get_data()
+
+    open_valves = set()
+    visited = {}
+    max_so_far = 0
+
+    def search(time, me, elephant, pressure):
+        nonlocal max_so_far
+
+        if visited.get((time, me, elephant), -1) >= pressure:
+            return
+
+        visited[time, me, elephant] = pressure
+
+        if time == 26:
+            max_so_far = max(max_so_far, pressure)
+            return
+
+        pressure += sum(rates[valve] for valve in open_valves)
+
+        # all open? just stay put...
+        if len(open_valves) == len(valves_to_open):
+            search(time + 1, me, elephant, pressure)
+            return
+
+        if me not in open_valves and rates[me]:
+            open_valves.add(me)
+
+            if elephant not in open_valves and rates[elephant]:
+                open_valves.add(elephant)
+                search(
+                    time + 1,
+                    me,
+                    elephant,
+                    pressure + rates[me] + rates[elephant]
+                )
+                open_valves.remove(elephant)
+
+            for neighbor in neighbors[elephant]:
+                search(
+                    time + 1,
+                    me,
+                    neighbor,
+                    pressure + rates[me]
+                )
+
+            open_valves.remove(me)
+
+        for me_neighbor in neighbors[me]:
+            if elephant not in open_valves and rates[elephant]:
+                open_valves.add(elephant)
+                search(
+                    time + 1,
+                    me_neighbor,
+                    elephant,
+                    pressure + rates[elephant]
+                )
+                open_valves.remove(elephant)
+
+            for elephant_neighbor in neighbors[elephant]:
+                search(
+                    time + 1,
+                    me_neighbor,
+                    elephant_neighbor,
+                    pressure,
+                )
+
+    search(1, 'AA', 'AA', 0)
+    print(max_so_far)
