42b464b92da6bffa08b7f26d33e801877b35da75
Alex Yu <yu.alex96@gmail.com>
2022-12-21 01:11:45 -0500

    [2022.21] z3

--- problems_2022/21.py
+++ problems_2022/21.py
@@ -1,6 +1,7 @@
 import operator
 
 import sympy
+import z3
 
 import utils
 
@@ -15,46 +16,76 @@ OPS = {
 }
 
 
-def get_data():
-    rows = utils.get_input(cast=str, delimiter=': ', line_delimiter='\n')
-    data = {}
+def get_jobs():
+    rows = utils.get_input(cast=str, delimiter=': ')
+    jobs = {}
 
     for monkey, job in rows:
         if job.isnumeric():
-            data[monkey] = int(job)
+            jobs[monkey] = int(job)
         else:
             monkey_1, op, monkey_2 = job.split()
-            data[monkey] = (OPS[op], monkey_1, monkey_2)
+            jobs[monkey] = (OPS[op], monkey_1, monkey_2)
 
-    return data
+    return jobs
 
 
-def evaluate(data, monkey):
-    if not isinstance(data[monkey], tuple):
-        return data[monkey]
+def evaluate(jobs, monkey):
+    if not isinstance(jobs[monkey], tuple):
+        return jobs[monkey]
 
-    op, monkey_1, monkey_2 = data[monkey]
-    return op(evaluate(data, monkey_1), evaluate(data, monkey_2))
+    op, monkey_1, monkey_2 = jobs[monkey]
+    return op(evaluate(jobs, monkey_1), evaluate(jobs, monkey_2))
 
 
 @utils.part
 def part_1():
-    data = get_data()
-    print(int(evaluate(data, 'root')))
+    jobs = get_jobs()
+    print(int(evaluate(jobs, 'root')))
 
 
 @utils.part
 def part_2():
-    data = get_data()
+    jobs = get_jobs()
     me = sympy.Symbol('humn')
-    _, monkey_1, monkey_2 = data['root']
+    _, monkey_1, monkey_2 = jobs['root']
 
-    data['humn'] = me
-    data['root'] = (
+    jobs['humn'] = me
+    jobs['root'] = (
         lambda monkey_1, monkey_2: sympy.solve(monkey_1 - monkey_2, me),
         monkey_1,
         monkey_2,
     )
 
-    solutions = evaluate(data, 'root')
+    solutions = evaluate(jobs, 'root')
     print(int(utils.assert_one(solutions)))
+
+
+@utils.part
+def part_2_z3():
+    jobs = get_jobs()
+    monkey_vars = {monkey: z3.Int(monkey) for monkey in jobs}
+    solver = z3.Optimize()
+
+    for monkey, job in jobs.items():
+        if monkey == 'humn':
+            continue
+
+        monkey_var = monkey_vars[monkey]
+
+        if isinstance(job, int):
+            solver.add(monkey_var == job)
+        else:
+            op, monkey_1, monkey_2 = job
+            monkey_1_var, monkey_2_var = monkey_vars[monkey_1], monkey_vars[monkey_2]
+
+            if monkey == 'root':
+                solver.add(monkey_1_var == monkey_2_var)
+            else:
+                solver.add(monkey_var == op(monkey_1_var, monkey_2_var))
+
+    me = monkey_vars['humn']
+    solver.minimize(me)
+    assert solver.check() == z3.sat
+
+    print(solver.model().eval(me).as_long())
